a7a96a4b5c33072d7c126e0aaaae1390873a58e7
최영훈 <c_yh0327@naver.com>
2020-06-06 01:54:41 +0900

    socket

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req,res)=> {
+  res.sendFile(__dirname+'/index.html');
 });
 
+router.use('/user',require('./user'));
 module.exports = router;
