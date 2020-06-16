6419f0e72a295ef57071d87ffc68e6e37aa2052f
최영훈 <c_yh0327@naver.com>
2020-06-16 15:40:39 +0900

    model

--- model/chat.js
+++ model/chat.js
@@ -3,7 +3,10 @@ var Schema = mongoose.Schema;
 
 var chatSchema = new Schema({
     chatUniversity:{type:String}, //학교 코드
-    chatForum:{type:String},
-    
+    chatForum:{type:String}, //수업 코드
+    chatMsg:[{
+        msgSender:{type: String, require:true},
+        msg:{type:String, require:true}
+    }]
 })
 module.exports = mongoose.model('chat',chatSchema)
--- model/university.js
+++ model/university.js
@@ -4,7 +4,10 @@ var Schema = mongoose.Schema;
 var universitySchema = new Schema({
     universityName : {type:String, required:true, unique:true},
     phone:{type:String, required:true},//대표 번호
-    department:{type:String, default:null},//학과
+    department:[{
+        departmentName:String,//학과 이름
+        departmentIdx:Number // 학과번호
+    }],//학과
     universityImage:String,
     universityCode:{type:String, unique:true},  
 })
